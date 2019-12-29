48566e86907abdce8ee8f5923e4c20d77a096bcc
Cristi Simion <cristisimion84@yahoo.co.uk>
2019-12-29 11:32:21 +0200

    Insert sort algoritm

--- SortarePrinInserare/SortarePrinInserare.cpp
+++ SortarePrinInserare/SortarePrinInserare.cpp
@@ -2,10 +2,60 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int *vector;
+int vectorSize;
+void citire()
+{
+	cout << "Cat de mare este vectorul";
+	cin >> vectorSize;
+	vector = new int[vectorSize];
+	for (int i = 0; i < vectorSize; i++)
+	{
+		cout << "Introduceti element in vector";
+		cin >> vector[i];
+	}
+
+}
+
+
+void afisare(int n, int v[])
+{
+	cout << "Vectorul este" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
+
+
+void sortareInserare(int sizeList, int vector[])
+{
+	for (int i = 1; i < sizeList; i++)
+	{
+		int elem = vector[i];
+		int j = i - 1;
+		while (j >= 0 && vector[j] > elem)
+		{
+			vector[j + 1] = vector[j];
+			j = j - 1;
+		}
+		vector[j + 1] = elem;
+
+	}
+
+}
 
 
 int main()
 {
+	citire();
+	sortareInserare(vectorSize, vector);
+	afisare(vectorSize, vector);
     return 0;
 }
 
